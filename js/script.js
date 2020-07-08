25144efa57312467f4ba5266f5f1a13d65fe3299
Alfonso <alfof87@gmail.com>
2020-07-08 16:49:19 +0200

    js-html-css-revolut

--- js/script.js
+++ js/script.js
@@ -1,47 +1,44 @@
 $(document).ready(function(){
-  $(".parent-dropdown").mouseover(function(){
-  $(this).children(".dropdown").toggle();
- });
- $(".parent-dropdown").mouseout(function(){
- $(this).children(".dropdown").toggle();
- });
-/////////////////////////////////////hamburger/X
- $(".fa-bars").click(function() {
-   $(".hidden-menu").show();
- });
+   $(".parent-dropdown").mouseover(function(){
+   $(this).children(".dropdown").toggle();
+   });
+   $(".parent-dropdown").mouseout(function(){
+   $(this).children(".dropdown").toggle();
+   });
+  /////////////////////////////////////hamburger/X
+   $(".fa-bars").click(function() {
+     $(".hidden-menu").show();
+   });
 
- $(".fa-times").click(function() {
-   $(".hidden-menu").hide();
- });
+   $(".fa-times").click(function() {
+     $(".hidden-menu").hide();
+   });
 
- $(".fa-bars").click(function() {
-   $(".fa-bars").hide();
-   $(".fa-times").show();
-   $(".submenu").hide();
+   $(".fa-bars").click(function() {
+     $(".fa-bars").hide();
+     $(".fa-times").show();
+     $(".submenu").hide();
 
- });
- $(".fa-times").click(function() {
-   $(".fa-times").hide();
-   $(".fa-bars").show();
- });
- ////////////////////////////////////sottomenu
+   });
+   $(".fa-times").click(function() {
+     $(".fa-times").hide();
+     $(".fa-bars").show();
+   });
+   ////////////////////////////////////sottomenu
 
- $(".parent1").click(function(){
- $(this).children(".submenu").toggle();
-});
-////////////chevron down/up////////
+   $(".parent1").click(function(){
+   $(this).children(".submenu").toggle();
+  });
+  ////////////chevron down/up////////
 
-$(".fa-bars").click(function() {
-  $(".fa-chevron-up").hide();
-  $(".fa-chevron-down").show();
-});
+  $(".fa-bars").click(function() {
+    $(".fa-chevron-up").hide();
+  });
+  $(".chevron-container").click(function(){
+  $(this).children(".fa-chevron-down").toggle();
+  });
+  $(".chevron-container").click(function(){
+  $(this).children(".fa-chevron-up").toggle();
+  });
 
-$(".chevron-container").click(function() {
-  $(".fa-chevron-down").hide();
-  $(".fa-chevron-up").show();
-});
-$(".chevron-container").click(function() {
-  $(".fa-chevron-up").hide();
-  $(".fa-chevron-down").show();
-});
 });
